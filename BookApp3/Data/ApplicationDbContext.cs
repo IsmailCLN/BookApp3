c46c47de8343a71bcac6b38093e66e8dedf27797
İsmail ÇELEN <81293395+IsmailCLN@users.noreply.github.com>
2023-06-06 14:20:26 +0300

    Add project files.

---
+++ BookApp3/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BookApp3.Models;
+
+namespace BookApp3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BookApp3.Models.Book>? Book { get; set; }
+    }
+}
---
+++ BookApp3/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookApp3.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string BookName { get; set; }
+        public String BookWriter { get; set; }
+        public int BookReview { get; set; }
+        
+        public Book() 
+        {
+        }   
+    }
+}
